d4167d62ae590106539e259a2f45012242698ef4
fikriAhmed <95493203+fikriAhmed@users.noreply.github.com>
2022-02-11 19:09:38 +0000

    My update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First class");
         System.out.println("2nd step");
+
+        System.out.println("3rd step");
+
     }
 }
